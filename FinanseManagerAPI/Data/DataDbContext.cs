a5674707c526017241275523e437a275c7c37792
ToNieJa <mateuszrudko@gmail.com>
2024-02-09 16:16:33 +0100

    Added new migration for Transactions

--- FinanseManagerAPI/Data/DataDbContext.cs
+++ FinanseManagerAPI/Data/DataDbContext.cs
@@ -1,15 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Security.Principal;
+﻿using FinanseManagerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace FinanseManagerAPI.Data
 {
     public class DataDbContext : DbContext
     {
-        public DataDbContext(DbContextOptions<DataDbContext> opt) : base(opt) 
-        {
-
-        }
-
+        public DataDbContext(DbContextOptions<DataDbContext> opt) : base(opt) { }
+        public DbSet<Transaction> Transactions { get; set; }
         
     }
+    
 }
--- FinanseManagerAPI/Models/Transaction.cs
+++ FinanseManagerAPI/Models/Transaction.cs
@@ -1,14 +1,17 @@
 ﻿using FinanseManagerAPI.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinanseManagerAPI.Models
 {
     public class Transaction
     {
-        public int Id { get; set; }
-        public int UserId { get; set; }
+        [Key]
+        public int Id { get; set; }  
         public string Description { get; set; }
         public decimal Amount {  get; set; }
         public DateTime Date { get; set; }
+        public int UserId { get; set; }
         public CategoryType Category { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public TransactionType Type { get; set; }
