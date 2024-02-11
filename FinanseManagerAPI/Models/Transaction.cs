d4a5dc6041f6051b1d6ba827a1a280c536db5182
ToNieJa <mateuszrudko@gmail.com>
2024-02-11 10:16:03 +0100

    Added validation to Transaction

--- FinanseManagerAPI/Models/Transaction.cs
+++ FinanseManagerAPI/Models/Transaction.cs
@@ -7,13 +7,19 @@ namespace FinanseManagerAPI.Models
     public class Transaction
     {
         [Key]
-        public int Id { get; set; }  
+        public int Id { get; set; }
+        [StringLength(30, MinimumLength = 0,ErrorMessage = "Description should have maximum 30 letters.")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "Transaction amount is required.")]
         public decimal Amount {  get; set; }
+        [Required(ErrorMessage = "Date is required.")]
         public DateTime Date { get; set; }
         public int UserId { get; set; }
+        [Required(ErrorMessage = "Category is required.")]
         public CategoryType Category { get; set; }
+        [Required(ErrorMessage = "Payment method is required.")]
         public PaymentMethod PaymentMethod { get; set; }
+        [Required(ErrorMessage = "Transaction type is required.")]
         public TransactionType Type { get; set; }
     }
 }
