a953326ce689580eb32c75115ef8389054084754
ToNieJa <mateuszrudko@gmail.com>
2024-03-06 10:14:29 +0100

    Added cors

--- FinanseManagerAPI/Program.cs
+++ FinanseManagerAPI/Program.cs
@@ -28,17 +28,29 @@ builder.Services.AddDbContext<DataDbContext>(opt =>
 builder.Services.AddIdentityCore<IdentityUser>()
                 .AddEntityFrameworkStores<AppDbContext>()
                 .AddApiEndpoints();
+//scoped
 builder.Services
     .AddScoped<ITransactions, Transactions>();
+//cors
+builder.Services.AddCors(p => p.AddPolicy("MyPolicy", build =>
+{
+    build.WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+}));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if(app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if(app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("MyPolicy");
 
 app.UseHttpsRedirection();
 
